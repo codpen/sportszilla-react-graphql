81476fe2f47c2a4e6a8654f81fb4ac4f6eef3156
codpen <davidpeng913@gmail.com>
2020-08-13 22:20:04 +0200

    chore: setting up Apollo

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,1 +1,19 @@
-console.info('Hello World!');
+import express from 'express';
+import { ApolloServer } from 'apollo-server-express';
+import cors from 'cors';
+import schema from './schema';
+
+const app = express();
+
+const server = new ApolloServer({
+  schema,
+  playground: true,
+});
+
+app.use('*', cors());
+
+server.applyMiddleware({ app, path: '/graphql' });
+
+app.listen({ port: 8000 }, () => {
+  console.log('Apollo Server on http://localhost:8000/graphql');
+});
--- server/src/sresolver.ts
+++
@@ -1,9 +0,0 @@
-import { IResolvers } from "graphql-tools";
-
-const resolvers: IResolvers = {
-  Query: {
-    helloWorld: () => "Hello world from Apollo Server",
-  },
-};
-
-export default resolvers;
