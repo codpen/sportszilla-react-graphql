4802c22da532bf6c9046b9348b796c88629ddd07
codpen <davidpeng913@gmail.com>
2020-08-14 16:10:40 +0200

    chore: install & setup dotenv

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,16 +1,19 @@
 import 'reflect-metadata';
 import { ApolloServer } from 'apollo-server';
 import { buildSchema } from 'type-graphql';
-
+import dotenv from 'dotenv';
 import { BookResolver } from './resolvers/BookResolver';
+
+dotenv.config();
+const { PORT } = process.env;
 
 async function main() {
   const schema = await buildSchema({
     resolvers: [BookResolver], // add this
   });
   const server = new ApolloServer({ schema });
-  await server.listen(4000);
-  console.info('Server has started!');
+  server.listen(PORT)
+    .then(() => console.info(`Server has started on port: ${PORT}!`));
 }
 
 main();
