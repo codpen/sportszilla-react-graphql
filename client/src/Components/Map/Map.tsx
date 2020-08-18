7c4e08f3a886efe8fc116ee0fb2b66be49d7c8c6
codpen <davidpeng913@gmail.com>
2020-08-18 17:12:56 +0200

    client/ add feature to change map

--- client/src/Components/Map/Map.tsx
+++ client/src/Components/Map/Map.tsx
@@ -3,25 +3,19 @@ import styles from './Map.module.scss';
 import { GoogleMap, LoadScript, Marker } from '@react-google-maps/api';
 
 import pinIcon from './footbal_icon.png';
+import mapStyle from './mapStyle.json';
 
 const containerStyle = {
   width: '100%',
   height: '100%',
 };
 
+const mapOptions = {
+  disableDefaultUI: true,
+  //styles: mapStyle,
+};
+
 const Map: React.FC = () => {
-  const [map, setMap] = React.useState(null);
-
-  const onLoad = React.useCallback(function callback(map) {
-    const bounds = new window.google.maps.LatLngBounds();
-    map.fitBounds(bounds);
-    setMap(map);
-  }, []);
-
-  const onUnmount = React.useCallback(function callback(map) {
-    setMap(null);
-  }, []);
-
   return (
     <div className={styles.Map} data-testid="Map">
       <LoadScript googleMapsApiKey={process.env.REACT_APP_API_KEY}>
@@ -29,7 +23,7 @@ const Map: React.FC = () => {
           center={{ lat: 51, lng: 0 }}
           zoom={14}
           mapContainerStyle={containerStyle}
-          options={{ disableDefaultUI: true }}
+          options={mapOptions}
         >
           <Marker title={'Marker'} position={{ lat: 51, lng: 0 }} />
           <Marker
