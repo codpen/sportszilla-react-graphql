f57dbb5c037a98e78bff98559e4c94c6db5bd1c2
codpen <davidpeng913@gmail.com>
2020-08-19 10:59:53 +0200

    feat: deleteEvent mutation

--- server/src/resolvers/EventResolver.ts
+++ server/src/resolvers/EventResolver.ts
@@ -1,4 +1,6 @@
-import { Resolver, Query, Mutation, Arg } from 'type-graphql';
+import {
+  Resolver, Query, Mutation, Arg,
+} from 'type-graphql';
 import Event from '../models/event.model';
 import NewSportEvent from '../inputs/NewSportEvent.input';
 import UpdateEvent from '../inputs/UpdateEvent.input';
@@ -6,9 +8,9 @@ import UpdateEvent from '../inputs/UpdateEvent.input';
 @Resolver()
 export default class EventResolver {
   @Query(() => Event)
-  async getOneEvent(@Arg('ID') id: number ) {
+  async getOneEvent(@Arg('ID') id: number) {
     try {
-      return Event.findOne({ where: { 'ID': id } });  
+      return Event.findOne({ where: { ID: id } });
     } catch (err) {
       console.error(err);
     }
@@ -17,7 +19,7 @@ export default class EventResolver {
   // @Query(() => Event)
   // async getAllEvents(){
   //   try {
-  //    return Event.findAll()  
+  //    return Event.findAll()
   //   } catch (error) {
   //     console.error(error)
   // }
@@ -30,39 +32,29 @@ export default class EventResolver {
     } catch (err) {
       console.error(err);
     }
-  } 
+  }
 
   @Mutation(() => Event)
   async updateEvent(@Arg('ID') id: number, @Arg('eventData') eventData: UpdateEvent) {
     try {
-     let event = await Event.findOne({ where: { 'ID': id } });
+      const event = await Event.findOne({ where: { ID: id } });
       if (event !== null) {
-      return event.update(eventData)}
+        return event.update(eventData);
+      }
     } catch (error) {
-      console.error(error)
+      console.error(error);
     }
   }
 
-  //   @Mutation(() => Event)
-  //   async updateEvent(@Arg('id') id: number, @Arg('eventData') eventData: UpdateEvent) {
-  //     try {
-  //       const event = await Event.findOne({ where: { 'id': id } });
-  //       if (!event) throw new Error('Event not found!');
-  //       return event.update(eventData);
-  //     } catch (err) {
-  //       console.error(err);
-  //     }
-  //   }
-
-  //   @Mutation(() => Boolean)
-  //   async deleteUser(@Arg('id') id: number) {
-  //     try {
-  //       const user = await User.findOne({ where: { 'id': id } });
-  //       if (!user) throw new Error('User not found!');
-  //       await user.destroy();
-  //       return true;
-  //     } catch (err) {
-  //       console.error(err);
-  //     }
-  //   }
+    @Mutation(() => Boolean)
+  async deleteEvent(@Arg('ID') id: number) {
+    try {
+      const event = await Event.findOne({ where: { 'ID': id } });
+      if (!event) throw new Error('Event not found!');
+      await event.destroy();
+      return true;
+    } catch (err) {
+      console.error(err);
+    }
+  }
 }
