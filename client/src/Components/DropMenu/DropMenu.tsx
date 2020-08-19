a226d492ac5f3bacb79d652980f503be8fb6f1c1
codpen <davidpeng913@gmail.com>
2020-08-19 12:20:16 +0200

    client/dropdownmenu

--- client/src/Components/DropMenu/DropMenu.tsx
+++ client/src/Components/DropMenu/DropMenu.tsx
@@ -1,5 +1,9 @@
 import React from 'react';
 import styles from './DropMenu.module.scss';
+import { Dropdown, Menu, Item, Trigger } from '@zendeskgarden/react-dropdowns';
+import { Button } from '@zendeskgarden/react-buttons';
+import { Row, Col } from '@zendeskgarden/react-grid';
+import { ReactComponent as ChevronIcon } from '@zendeskgarden/svg-icons/src/16/chevron-down-stroke.svg';
 
 const DropMenu: React.FC = () => (
   <div className={styles.DropMenu} data-testid="DropMenu">
