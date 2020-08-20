695af39bf2d040089fc27184f657ae81e38d61b9
codpen <davidpeng913@gmail.com>
2020-08-20 12:37:08 +0200

    feat(FE): merge Login / SignUp -> Join

---
+++ client/src/Components/User/Join/Join.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import styles from './Join.module.scss';
+
+const Join: React.FC = () => (
+  <div className={styles.Join} data-testid="Join">
+    Join Component
+  </div>
+);
+
+export default Join;
