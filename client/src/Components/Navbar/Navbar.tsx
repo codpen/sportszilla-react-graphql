880dc4f843a927073f49f2cfddc45778e35a49d1
codpen <davidpeng913@gmail.com>
2020-08-21 22:02:09 +0200

    fix: logo

--- client/src/Components/Navbar/Navbar.tsx
+++ client/src/Components/Navbar/Navbar.tsx
@@ -6,14 +6,15 @@ import logo from '../../Images/godzilla.svg';
 import logoBall from '../../Images/filled-circle.svg';
 import hamburger from '../../Images/open-menu.svg';
 import DropMenu from '../DropMenu/DropMenu';
+import logo1 from '../../Images/Logo/LogoMakr_7xg4JO.png'
 
 const Navbar: React.FC = () => {
   return (
     <div className={styles.Navbar} data-testid="Navbar">
       <div className={styles.logoContainer}>
         <Link className={styles.NavbarLeft} to="/">
-          <img className={styles.NavbarLeft_logo_img} src={logo} alt="logo" />
-          <img className={styles.NavbarLeft_logo_ball} src={logoBall} alt="logoBall" />
+          <img className={styles.NavbarLeft_logo_img} src={logo1} alt="logo" />
+          {/* <img className={styles.NavbarLeft_logo_ball} src={logoBall} alt="logoBall" /> */}
         </Link>
       </div>
       <div className={styles.NavbarRight}>
