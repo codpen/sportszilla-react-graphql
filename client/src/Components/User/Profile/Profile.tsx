58c9e9d4a1ab7aa39fd6a459a3cdfa40781b86ae
codpen <davidpeng913@gmail.com>
2020-08-21 12:22:08 +0200

    develop/ shading box

--- client/src/Components/User/Profile/Profile.tsx
+++ client/src/Components/User/Profile/Profile.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, Dispatch, SetStateAction } from 'react';
 import { PALETTE } from '@zendeskgarden/react-theming';
 import { Avatar } from '@zendeskgarden/react-avatars';
 import { ReactComponent as UserIcon } from '@zendeskgarden/svg-icons/src/16/user-solo-stroke.svg';
@@ -6,51 +6,20 @@ import { ReactComponent as UserIcon } from '@zendeskgarden/svg-icons/src/16/user
 import ButtonGeneric from '../../ButtonGeneric/ButtonGeneric';
 import EventLogin from '../../EventLogin/EventLogin';
 import styles from './Profile.module.scss';
-import { UserData } from '../UserData';
 
 import Data from '../../../mockData/data.json';
 import image from '../../../Images/profile.jpg';
 import { element } from 'prop-types';
-
-type Event = {
-  id: number;
-  sport_id: number;
-  location: {
-    latitude: number;
-    longitude: number;
-    accuracy: number;
-  };
-  date: string;
-  description: string;
-  organizer: number;
-  filter: {
-    target_gender: string;
-    target_level: string;
-  };
-  time_start: string;
-  time_end: string;
-  registered_participants: number[];
-  max_participants: number;
-  min_participants: number;
-};
-
-type User = {
-  id: number;
-  first_name: string;
-  last_name: string;
-  date_birth: string;
-  sport_interest: number[];
-  Location: { latitude: number; longitude: number; accuracy: number };
-  gender: string;
-};
+import { UserData } from '../UserData';
 
 interface PropTypes {
-  user: User | undefined;
+  user: UserData;
+  setUser: Dispatch<SetStateAction<UserData>>;
 }
 
-const Profile: React.FC<PropTypes> = ({ user }) => {
+const Profile: React.FC<PropTypes> = ({ user, setUser }) => {
   const [event, setEvent] = useState<Event[]>(Data.events);
-
+  const mockUser = Data.users[0];
   const listFriends = Data.users.map((user) => {
     return (
       <li>
@@ -68,16 +37,15 @@ const Profile: React.FC<PropTypes> = ({ user }) => {
     <div className={styles.Profile} data-testid="Profile">
       <div className={styles.spacer}></div>
       <div className={styles.avatarCnt}>
-        <Avatar backgroundColor={PALETTE.grey[600]} size="large">
-          <Avatar.Text>{user?.first_name[0]}</Avatar.Text>
+        <Avatar backgroundColor={PALETTE.grey[600]} style={{ height: '70px', width: '70px' }}>
+          <Avatar.Text>{mockUser?.first_name[0]}</Avatar.Text>
         </Avatar>
         <h2>
-          {user?.first_name} {user?.last_name}
+          {mockUser?.first_name} {mockUser?.last_name}
         </h2>
       </div>
       <div className={styles.btnCnt}>
         <ButtonGeneric buttonText="Create Event" buttonLink="/event/" />
-        <ButtonGeneric buttonText="Join" buttonLink="/board/" />
       </div>
       <div className={styles.eventCtn}>
         <EventLogin event={event[0]} />
