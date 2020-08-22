5a5cb1cb5270bfe5002ff72def2e77a6e4322093
codpen <davidpeng913@gmail.com>
2020-08-22 15:14:59 +0200

    test: added sequelize-mock devdependencies

--- server/src/__tests__/user.model.test.ts
+++ server/src/__tests__/user.model.test.ts
@@ -1,8 +1,16 @@
 import User from '../models/user.model';
 
-const user = new User();
-test('should have firstName');
-expect(user).toBeInstanceOf(User);
+const BCRYPT_HASH_BEGINNING = '$2a$';
+const TEST_PASSWORD = 'hello there';
+
+User.create({ passW: TEST_PASSWORD }).then( function( user ){
+  if( !user ) throw new Error( 'User is null' )
+  if( !user.passW ) throw new Error( 'Password was not saved' )
+  if( user.passW === TEST_PASSWORD )
+    throw new Error( 'Password is plaintext' )
+  if( user.passW.indexOf( BCRYPT_HASH_BEGINNING ) === -1 )
+    throw new Error( 'Password was not encrypted' )
+})
 
 // console.log('User', user)
 
