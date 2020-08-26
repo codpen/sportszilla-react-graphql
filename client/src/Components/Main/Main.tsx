40760543a0a180892984d8e083d798b65a1085a6
codpen <davidpeng913@gmail.com>
2020-08-26 17:08:51 +0200

    feat: lazy loading, and fallback screen while loading

--- client/src/Components/Main/Main.tsx
+++ client/src/Components/Main/Main.tsx
@@ -1,11 +1,11 @@
-import React, { useState, useEffect, ReactElement } from 'react';
+import React, { useState, useEffect, ReactElement, lazy, Suspense } from 'react';
 import { Switch, Redirect, Route } from 'react-router-dom';
 import Loader from '../Loader/Loader';
 import { useQuery, useLazyQuery, gql } from '@apollo/client';
 import Apollo from '../ApolloClient/Apollo';
-import { LoginRequest, LoginResp } from '../User/LoginRequest';
-import Navbar from '../Navbar/Navbar';
-import Intro from '../Intro/Intro';
+import './Main.scss';
+// import Navbar from '../Navbar/Navbar';
+// import Intro from '../Intro/Intro';
 import Join from '../User/Join/Join';
 import Login from '../User/Login/Login';
 import SignUp from '../User/SignUp/SignUp';
@@ -16,7 +16,8 @@ import Board from '../Board/Board';
 import CreateEvent from '../CreateEvent/CreateEvent';
 import { UserData } from '../User/UserData';
 import { EventData } from '../Board/Event';
-import './Main.scss';
+const Navbar = lazy(() => import('../Navbar/Navbar'));
+const Intro = lazy(() => import('../Intro/Intro'))
 
 function Main(): ReactElement {
   interface Response {
@@ -42,22 +43,6 @@ function Main(): ReactElement {
     }
   `;
 
-  const loginRequest: LoginRequest<LoginResp> = (loginData, path) => {
-    const loginURL = `http://localhost:8000/auth/${path}`;
-    const init: RequestInit = {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      mode: 'cors',
-      body: JSON.stringify(loginData),
-    };
-    return fetch(loginURL, init)
-      .then((result) => (result.status >= 400 ? Promise.reject(result) : result))
-      .then((result) => result.json())
-      .catch(console.error);
-  };
-
   const { loading, data, error } = useQuery<Response>(EVENTS);
 
   const [users, setUsers] = useState<UserData[]>([]);
@@ -69,6 +54,7 @@ function Main(): ReactElement {
     console.log(data.getAllEvents);
     return (
       <div className="Main">
+        <Suspense fallback={<div>Loading...</div>}>
         <header>
           <Navbar />
         </header>
@@ -90,13 +76,13 @@ function Main(): ReactElement {
               <EventDetails />
             </Route>
             <Route exact path="/user/join/">
-              <Join setUser={setLoggedInUser} loginRequest={loginRequest} />
+              <Join />
             </Route>
             <Route exact path="/user/login/">
-              <Login setUser={setLoggedInUser} loginRequest={loginRequest} />
+              <Login setUser={setLoggedInUser} />
             </Route>
             <Route exact path="/user/signup/">
-              <SignUp setUser={setLoggedInUser} loginRequest={loginRequest} />
+              <SignUp setUser={setLoggedInUser} />
             </Route>
             <Route exact path="/user/profile/">
               <Profile user={undefined} setUser={setLoggedInUser} />
@@ -104,6 +90,7 @@ function Main(): ReactElement {
             <Redirect from="/" to="/intro/" />
           </Switch>
         </main>
+        </Suspense>
       </div>
     );
   }
